a7638c6438b3473c2b430371ad68ae2e2d2432f8
Amelia Ho <120755144+yh63935@users.noreply.github.com>
2023-09-06 21:35:22 -0700

    Update game to be played with buttons and to stop at a score of 5.

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-//declare variables
+
 let playerScore = 0
 let computerScore = 0
 
-//randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’
+// Randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’ for computer's turn
 function getComputerChoice() {
     const choices = ['Rock', 'Paper' , 'Scissors']
     //get random index from array
     let randomIndex = (Math.floor(Math.random() * 3))
+    //get random choice from array
     let randomTurn = choices[randomIndex]
     return randomTurn
 }
 
-//plays a round of rock, paper, scissors and determines who wins
+// Plays a round of rock, paper, scissors and determines who wins
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
     computerSelection = computerSelection.toLowerCase()
     if (playerSelection === computerSelection) {
-        console.log("It's a draw!")
+        console.log("It's a draw for this round!")
     }
     else if((playerSelection === 'rock' && computerSelection === 'scissors')
             || (playerSelection === 'paper' && computerSelection === 'rock')
             || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        playerScore++
-        console.log('You win!')
+        playerScore++;
+        console.log('You win the round!')
     }
     else if (playerSelection !== 'rock' && playerSelection !== 'scissors'
             && (playerSelection !== 'paper')) {
@@ -30,27 +31,52 @@ function playRound(playerSelection, computerSelection) {
     }
     else {
         computerScore++;
-        console.log('You lose :(')
+        console.log('You lose this round:(')
     }
 }
 
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+const playerSelectionBtns = document.querySelectorAll("button");
 
-//runs a game consisting of 5 rounds
-function game() {
-    for (let i =0; i<5; i++) {
-        playRound(prompt('Choose your weapon'), getComputerChoice())
-    }
-    if (playerScore>computerScore) {
-        console.log('You win!')
-    }
-    else if (playerScore===computerScore) {
-        console.log('A draw! Play again?')
-    }
-    else {
-        console.log('You lose :(')
-    }
+// Play a game of rock, paper, scissors that ends when player or computer reaches 5 points
+function game() {        
+            playerSelectionBtns.forEach(btn => {
+                btn.addEventListener("click", () => {
+                    if (playerScore < 5 && computerScore < 5) {
+
+                        playRound(btn.innerText, getComputerChoice())
+                    }
+                    else {
+                        if (playerScore>computerScore) {
+                            console.log('You win!')
+                        }
+                        else if (playerScore===computerScore) {
+                            console.log('A draw! Play again?')
+                        }
+                        else {
+                            console.log('You lose :(')
+                        }
+                    }
+                })
+            })
 }
 game()
 console.log(playerScore)
 console.log(computerScore)
 
+
+
+//     Add a div for displaying results and change all of your console.logs into DOM methods.
+//     Display the running score, and announce a winner of the game once one player reaches 5 points.
+//     You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
+
+// Once you’re all done with your UI and made sure everything’s satisfactory, ensure all of your changes are committed to the rps-ui branch with git status before continuing.
+// Now let’s take a look at how we can merge the changes from our rps-ui branch back to our main branch.
+
+//     Checkout the branch we want to merge INTO i.e. main with the command git checkout main.
+//     Now let’s merge our rps-ui branch into main, our current branch, with git merge rps-ui.
+//     If everything goes fine, our rps-ui branch is now successfully merged with main! Use git log and you’ll see all the commits you’ve made to your feature branch on top of the commits you made to the main branch. Now for our final step!
+//     Let’s push our main branch into our remote repo by running git push origin main . Go to your GitHub repo and you’ll see that our main branch will have all the changes and commits you made to the rps-ui branch. Congratulations! You’ve successfully pushed your first feature into your production branch!
+//     Now that we have all our code in the main branch, we don’t really need our rps-ui branch anymore. Let’s do some cleanup, both locally and in the remote repo. Delete the branch from our local repo with git branch -d rps-ui and also delete it from the remote repo on GitHub with git push origin --delete rps-ui. Congrats, we’re all done with our cleanup!
